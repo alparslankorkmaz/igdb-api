db4bcc1801612c796d84673e67d6f2f0da5ffcec
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2024-02-01 18:15:05 +0000

    add icy glass to game card

--- app/components/Game.js
+++ app/components/Game.js
@@ -3,19 +3,34 @@ import RatingDisplay from "./RatingDisplay";
 
 export default function Game({ ...props }) {
   return (
-    <div key={props.id} className="flex flex-col w-64 h-auto gap-5">
-      <Link href={`/${props.slug}`} className="">
-        <img
-          src={`https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${props.cover.image_id}.jpg`}
-          alt=""
-        />
-      </Link>
-      <div className="">
-        <div className="flex justify-between">
-          <Link href={`/${props.slug}`} className="w-2/3">
-            {props.name}
-          </Link>
-          <RatingDisplay rating={props.rating} />
+    <div class="relative overflow-hidden hover:scale-105 duration-200">
+      {/* ICY GLASS BG IMAGE */}
+      <img
+        src={`https://images.igdb.com/igdb/image/upload/t_cover_small_2x/${props.cover.image_id}.jpg`}
+        alt={props.name}
+        className="absolute object-cover w-full pt-7 -z-10 blur-md"
+      />
+
+      <div key={props.id} className="flex flex-col">
+        <Link href={`/${props.slug}`}>
+          <img
+            src={`https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${props.cover.image_id}.jpg`}
+            alt={props.name}
+          />
+        </Link>
+
+        <div className="">
+          <div className="flex flex-col justify-around h-36 p-2">
+            <div className="flex items-center justify-between">
+              <Link
+                href={`/${props.slug}`}
+                className="w-2/3 text-sm text-white"
+              >
+                {props.name}
+              </Link>
+              <RatingDisplay rating={props.rating} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/top-100-games/page.js
+++ app/top-100-games/page.js
@@ -6,10 +6,12 @@ export default async function page() {
   // console.log(highRatedGamesData);
   return (
     <>
-      <div className="flex flex-wrap justify-center gap-5  ">
+      <div className="flex  flex-wrap justify-center gap-5 my-5">
         {highRatedGamesData.length > 0 &&
           highRatedGamesData.map(({ ...props }) => (
-            <Game key={props.id} {...props} />
+            <div className="w-52 h-auto" key={props.id}>
+              <Game {...props} />
+            </div>
           ))}
       </div>
     </>
